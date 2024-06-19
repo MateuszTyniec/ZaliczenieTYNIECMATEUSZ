165068e8a0797fda39436c0b56686368532e2698
MateuszTyniec <161605903+MateuszTyniec@users.noreply.github.com>
2024-06-19 12:48:41 +0000

    Zaliczenie Tyniec Mateusz

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,36 @@
+import java.util.Scanner;
+
+public class Main
+{
+  public static void main(String[] args)
+  {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Podaj rozmiar tablicy: ");
+        int rozmiar = scanner.nextInt();
+        int[] tablica = new int[rozmiar];
+
+        System.out.println("Wprowadź liczby całkowite:");
+        for (int i = 0; i < rozmiar; i++) {
+            tablica[i] = scanner.nextInt();
+        }
 
 
-public class Main {
-  public static void main(String[] args) {
-    System.out.println("Hello world!");
-  }
+      
 
+        for (int i = 0; i < rozmiar - 1; i++) {
+            for (int j = 0; j < rozmiar - i - 1; j++) {
+                if (tablica[j] > tablica[j + 1]) 
+                {
+                    int temp = tablica[j];
+                    tablica[j] = tablica[j + 1];
+                    tablica[j + 1] = temp;
+                }
+            }
+        }
 
+        System.out.println("Posortowana liczby:");
+        for (int i = 0; i < rozmiar; i++) {
+            System.out.print(tablica[i] + " ");
+        }
+    }
 }
